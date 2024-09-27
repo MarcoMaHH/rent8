00db60d15659ba47d1f9e1a126ac0f032f0adbce
MarcoMaHH <407278890@qq.com>
2024-09-27 09:32:38 +0800

    mark

--- database/seeds/AdminUser.php
+++ database/seeds/AdminUser.php
@@ -19,7 +19,7 @@ class AdminUser extends Seeder
         $this->table('admin_user')->insert([
             ['id' => 1, 'admin_role_id' => 1, 'username' => 'admin', 'expiration_date' => date("Y-m-d H:i:s", strtotime(" +1 year")),
              'password' => $password, 'salt' => $salt],
-            ['id' => 2, 'admin_role_id' => 3, 'username' => 'user', 'expiration_date' => date("Y-m-d H:i:s", strtotime(" +1 year")),
+            ['id' => 2, 'admin_role_id' => 2, 'username' => 'user', 'expiration_date' => date("Y-m-d H:i:s", strtotime(" +1 year")),
              'password' => $password, 'salt' => $salt]
         ])->save();
     }
