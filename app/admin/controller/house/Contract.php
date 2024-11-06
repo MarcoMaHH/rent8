851e935e84fe075b4f3ed8cb1a384f92cc4e9776
君常笑 <407278890@qq.com>
2024-11-06 17:56:05 +0800

    mark

--- app/admin/controller/house/Contract.php
+++ app/admin/controller/house/Contract.php
@@ -5,7 +5,6 @@ namespace app\admin\controller\house;
 use app\admin\controller\Common;
 use app\admin\model\HouseContract as ContractModel;
 use app\admin\model\HouseOther as OtherModel;
-use app\admin\model\HouseProperty as PropertyModel;
 use app\admin\model\BillSum as SumModel;
 use app\admin\library\Property;
 use think\facade\View;
@@ -29,11 +28,11 @@ class Contract extends Common
         }
         $count = ContractModel::alias('a')->where($conditions)->count();
         $numbers = ContractModel::alias('a')
-        ->leftJoin('HouseProperty b', 'a.house_property_id = b.id')
-        ->field('a.*,b.name as property_name')
-        ->where($conditions)
-        ->order(['a.accout_mark','a.accounting_date' => 'desc'])
-        ->select();
+            ->leftJoin('HouseProperty b', 'a.house_property_id = b.id')
+            ->field('a.*,b.name as property_name')
+            ->where($conditions)
+            ->order(['a.accout_mark', 'a.accounting_date' => 'desc'])
+            ->select();
         foreach ($numbers as $value) {
             if ($value['accounting_date']) {
                 $value['accounting_date'] = \substr($value['accounting_date'], 0, 10);
@@ -58,27 +57,20 @@ class Contract extends Common
     public function save()
     {
         $id = $this->request->post('id/d', 0);
-        $house_property_id = $this->request->post('house_property_id/s', '', 'trim');
-        if (!$propertyArr = PropertyModel::find($house_property_id)) {
-            return $this->returnError('房产数据异常');
-        }
         $data = [
             'house_property_id' => $this->request->post('house_property_id/s', '', 'trim'),
-            'type' => $this->request->post('type/s', '', 'trim'),
-            'total_money' => $this->request->post('total_money/d', 0),
-            'note' => $this->request->post('note/s', '', 'trim'),
-            'circulate_mark' => $this->request->post('circulate_mark/s', '', 'trim'),
-            'accounting_date' => $this->request->post('accounting_date/s', '', 'trim'),
-            'accout_mark' => 'N'
+            'house_number_id' => $this->request->post('house_number_id/s', '', 'trim'),
+            'start_date' => $this->request->post('start_date/s', '', 'trim'),
+            'end_date' => $this->request->post('end_date/s', '', 'trim'),
         ];
         if ($id) {
-            if (!$electricity = OtherModel::find($id)) {
-                return $this->returnError('修改失败，记录不存在。');
+            if (!$electricity = ContractModel::find($id)) {
+                return $this->returnError('修改失败，合同不存在。');
             }
             $electricity->save($data);
             return $this->returnSuccess('修改成功');
         }
-        OtherModel::create($data);
+        ContractModel::create($data);
         return $this->returnSuccess('添加成功');
     }
 
