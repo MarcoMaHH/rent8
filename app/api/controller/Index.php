14643c81f08859e98cf28178e68f09559142d085
君常笑 <407278890@qq.com>
2024-11-09 21:56:23 +0800

    mark

--- app/api/controller/Index.php
+++ app/api/controller/Index.php
@@ -3,6 +3,7 @@
 namespace app\api\controller;
 
 use app\admin\model\HouseProperty as PropertyModel;
+use app\admin\model\HouseBilling as BillingModel;
 use app\admin\model\BillSum as SumModel;
 
 class Index extends Common
@@ -16,17 +17,24 @@ class Index extends Common
         }, $property);
         $accounting_month = date('Y-m');
         $income = SumModel::where('house_property_id', 'in', $result)
-        ->where('type', TYPE_INCOME)
-        ->where('accounting_date', $accounting_month)
-        ->sum('amount');
+            ->where('type', TYPE_INCOME)
+            ->where('accounting_date', $accounting_month)
+            ->sum('amount');
         $spending = SumModel::where('house_property_id', 'in', $result)
-        ->where('type', TYPE_EXPENDITURE)
-        ->where('accounting_date', $accounting_month)
-        ->sum('amount');
+            ->where('type', TYPE_EXPENDITURE)
+            ->where('accounting_date', $accounting_month)
+            ->sum('amount');
+        $threeDaysAgo = date('Y-m-d H:i:s', strtotime('+3 days'));
+        $bill = BillingModel::where('house_property_id', 'in', $result)
+            ->where('accounting_date', null)
+            ->where('meter_reading_time', '=', '')
+            ->where('start_time', '<', $threeDaysAgo)
+            ->count();
         $house_info = [
             'income' => $income,
             'spending' => $spending,
             'profit' => $income - $spending,
+            'bill' => $bill,
         ];
         return $this->returnWechat($house_info);
     }
